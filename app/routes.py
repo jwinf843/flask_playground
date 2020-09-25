e1f67b769e3fc976284a0226d71de760fc273889
Jordan <jwinf843@gmail.com>
2020-09-25 11:26:03 +0900

    Introduced template rendering and created a template for the homepage.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {
+        'username': 'jwinf843',
+    }
+
+    return render_template('index.html', title='Home', user=user)
+
+app.run(debug=True)
